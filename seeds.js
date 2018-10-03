6a196e886ae7f27654a1c061551ed70127668a70
Burrito Scott <scottchapman@Scotts-MacBook-Pro.local>
2018-10-02 19:22:14 -0700

    initaliazed npm  packages, created the seeds.js animals with collection pets, cloned over a server.js file to manipulate

---
+++ seeds.js
@@ -0,0 +1,20 @@
+// use animals
+
+db.pets.insertMany([
+    {name: "Zoey",
+    type: "Dog"},
+    {name: "Norman",
+    type: "Dog"},
+    {name: "Oprah",
+    type: "Cat"},
+    {name: "Ethan",
+    type: "Cat"},
+    {name: "Victor",
+    type: "Lizard"},
+    {name: "Lucy",
+    type: "Cat"},
+    {name: "Polly",
+    type: "Bird"},
+    {name: "Buggy",
+    type: "Lizard"} 
+]);
